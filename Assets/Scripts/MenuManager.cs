48c3572b00fe817e64c4afac980724dc66aedcbd
BogoxD <91665508+BogoxD@users.noreply.github.com>
2024-09-08 12:15:18 +0100

    fixes

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,14 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject mainMenu;
+    [SerializeField] GameObject settingsMenu;
+
     public void OnPlay()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void OnSettings()
+    {
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
+    public void OnBack()
+    {
+        mainMenu.SetActive(true);
+        settingsMenu.SetActive(false);
     }
     public void OnQuit()
     {
@@ -23,5 +31,9 @@ public class MenuManager : MonoBehaviour
     {
         SceneManager.LoadScene(0);
         GameManager.instance.RestartSpawning();
+    }
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex, true);
     }
 }
